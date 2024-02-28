0024e80919a44564f00302c7394219d29dd5db4c
Marcos Fernández Avendaño <113597220+marcosfa00@users.noreply.github.com>
2024-02-28 09:31:59 +0100

    Cliente

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,17 @@
+import java.io.IOException;
+import java.sql.SQLOutput;
+
 public class Main {
-    public static void main(String[] args) {
-       Methods m = new Methods();
-       User user = new User();
-       m.createUser(user);
+    public static void main(String[] args) throws IOException {
+        Methods m = new Methods();
+        User user = new User();
+        System.out.println("A que IP te vas a conectar ?");
+        m.setServerIP( Methods.sc.nextLine());
+        System.out.println();
+        System.out.println("Indica el puerto al que te vas a conectar de la ip: " + m.getServerIP());
+        m.setPort(Methods.sc.nextInt());
+        m.initComponents();
+        m.createUser(user);
 
     }
 }
--- src/Methods.java
+++ src/Methods.java
@@ -1,24 +1,51 @@
+
+
 import java.io.*;
 import java.net.Socket;
 import java.util.Scanner;
 
 public class Methods {
     //Conexión con el Servidor
-    private final String serverIP = "192.168.64.16";
-    private final int Port = 6666;
+    //"192.168.64.16"
+    private String serverIP ;
+    //6666
+    private  int Port ;
     private Socket socket;
     DataInputStream dataInputStream;
     DataOutputStream dataOutputStream;
 
     //Constructor
-    public Methods() throws IOException {
+    public Methods() {
+
+    }
+
+    public void initComponents()throws IOException {
         this.socket = new Socket(serverIP,Port);
         this.dataOutputStream = new DataOutputStream(socket.getOutputStream());
         this.dataInputStream = new DataInputStream(socket.getInputStream());
     }
 
+    public void setServerIP(String ip){
+        this.serverIP = ip;
+    }
+    public String getServerIP() {
+        return serverIP;
+    }
+
+    public void setPort(int port){
+        this.Port = port;
+    }
+
+    public int getPort() {
+        return Port;
+    }
+
+
+
     //Pedir datos consola
-    Scanner sc = new Scanner(System.in);
+    public static Scanner sc = new Scanner(System.in);
+
+
 
     /**
      * Esta función le pide al usuario el nombre d eusuario para poder registrarse
@@ -31,21 +58,44 @@ public class Methods {
         System.out.println("Bienvenido " + user.getUsername());
         System.out.println("A continuación puedes comenzar a escribir: ");
         String msg ="";
+        String recibido ="";
         do {
+            System.out.println("nuevo mensaje:");
             msg = sc.nextLine();
             enviarMensaje(user,msg);
 
+            recibido = recibirMensaje();
 
-        }while (!msg.equals("0"));
+
+        }while (!recibido.equals(" /bye"));
+        System.out.println("Lamentamos que te vayas :(  puedes volver cuando quieras :D");
+        this.closeConnection();
     }
 
     private void enviarMensaje(User user,String msg) throws IOException {
+
         String mensajeTotal = user.getUsername() + ": " + msg;
         byte[] mensaje = mensajeTotal.getBytes();
         dataOutputStream.writeInt(mensaje.length);
         dataOutputStream.write(mensaje);
+
     }
 
+    public String recibirMensaje()throws IOException{
+        int longitudMensaje = dataInputStream.readInt();//Espera a que lelgue un mensage de tipo int
+        byte[] mensajeBytes = new byte [longitudMensaje];
+        dataInputStream.readFully(mensajeBytes);
+        String operacion = new String(mensajeBytes);
+        System.out.println("La operacion que nos han enviado es: " + operacion);
+        return operacion;
+
+    }
+
+    public void closeConnection() throws IOException {
+        dataOutputStream.close();
+        dataInputStream.close();
+        socket.close();
+    }
 
 
 
@@ -54,3 +104,4 @@ public class Methods {
 
 
 }
+
